6269ea7314f8066716bc6f33d7fb6d830c25ddcd
Albert O'Connor <amjoconn@gmail.com>
2011-02-19 21:27:04 -0500

    Initial check of jautocomplete mainly to support the fass50 project.

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+# Use setuptools if we can
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='jautocomplete',
+    version="0.0.1",
+    description='django-jquery-autocomplete give you easy autocomplete in the admin or in your forms.',
+    long_description='Working with the jquery automcomplete plugin to create simple or complex autocomplete including in the admin, in forms, with ajax and non ajax flavors.',
+    author='Albert O\'Connor',
+    author_email='info@albertoconnor.ca',
+    url='http://albertoconnor.ca',
+    download_url='',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+    packages=[
+        'jautocomplete'
+    ],
+)
